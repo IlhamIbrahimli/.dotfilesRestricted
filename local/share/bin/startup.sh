782af50faa8c67f93bb3825d9a86ac55f79b2859
Ilham Ibrahimli <78649705+IlhamIbrahimli@users.noreply.github.com>
2024-11-30 20:32:54 +0000

    Change name to .local
    Merge with the .local folder in the home directory.

---
+++ local/share/bin/startup.sh
@@ -0,0 +1,6 @@
+rm -rf /home/testuser/.mozilla/firefox/j03qml8y.default-release/cookies.sqlite
+rm -rf /home/testuser/.mozilla/firefox/j03qml8y.default-release/cookies.sqlite-wal
+rm -rf /home/testuser/.mozilla/firefox/j03qml8y.default-release/places.sqlite
+rm -rf /home/testuser/.mozilla/firefox/j03qml8y.default-release/places.sqlite-wal
+rm -rf /home/testuser/.mozilla/firefox/j03qml8y.default-release/sessionstore.jsonlz4
+rm -rf /home/testuser/.mozilla/firefox/j03qml8y.default-release/sessionstore-backups
