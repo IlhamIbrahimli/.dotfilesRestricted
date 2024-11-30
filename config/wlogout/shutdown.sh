2c314050df98c057edb9fdd4b0b4196b4f89262f
Ilham Ibrahimli <78649705+IlhamIbrahimli@users.noreply.github.com>
2024-11-30 20:27:11 +0000

    Rename this folder to .config in your install

---
+++ config/wlogout/shutdown.sh
@@ -0,0 +1,5 @@
+rm -rf /home/testuser/.mozilla/firefox/j03qml8y.default-release/cookies.sqlite
+rm -rf /home/testuser/.mozilla/firefox/j03qml8y.default-release/cookies.sqlite-wal
+rm -rf /home/testuser/.mozilla/firefox/j03qml8y.default-release/places.sqlite
+rm -rf /home/testuser/.mozilla/firefox/j03qml8y.default-release/places.sqlite-wal
+systemctl poweroff
